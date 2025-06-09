8bff73864fc0493cc3c8a2f6d55aa30689079e39
Michael Iglesias <newfrontierpropertymgmt@protonmail.com>
2025-06-09 10:48:54 -0400

    chore: add verbose logging to wait_for_block

--- crates/federation/src/bitcoin_stream.rs
+++ crates/federation/src/bitcoin_stream.rs
@@ -122,6 +122,7 @@ impl BitcoinCore {
             match self.rpc.get_block_hash(height.into()) {
                 Ok(hash) => {
                     let info = self.rpc.get_block_info(&hash)?;
+                    info!("wait_for_block: block {} exists with hash {} and confirmations {}", height, hash, info.confirmations);
                     if info.confirmations >= num_confirmations as i32 {
                         return Ok(self.rpc.get_block(&hash)?);
                     } else {
@@ -134,6 +135,7 @@ impl BitcoinCore {
                         == BitcoinRpcError::RpcInvalidParameter =>
                 {
                     // block does not exist yet
+                    warn!("block does not exist yet, retrying...");
                     tokio::time::sleep(RETRY_DURATION).await;
                     continue;
                 }
